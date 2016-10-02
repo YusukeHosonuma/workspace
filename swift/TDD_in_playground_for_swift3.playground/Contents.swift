f680f019691d3cffcbdae06a521be20f4a3c3b84
Yusuke Hosonuma <tobi462@gmail.com>
2016-10-02 14:50:12 +0900

    Playground で XCTest を実行するサンプル（Swift 3.0向け）を追加

---
+++ swift/TDD_in_playground_for_swift3.playground/Contents.swift
@@ -0,0 +1,46 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+import XCTest
+
+
+//: ## Test Foundation
+
+class PlaygroundTestObserver : NSObject, XCTestObservation {
+    @objc func testCase(_ testCase: XCTestCase, didFailWithDescription description: String, inFile filePath: String?, atLine lineNumber: UInt) {
+        print("Test failed on line \(lineNumber): \(testCase.name), \(description)")
+    }
+}
+
+let observer = PlaygroundTestObserver()
+let center = XCTestObservationCenter.shared()
+center.addTestObserver(observer)
+
+struct TestRunner {
+    
+    func run(_ testClass: AnyClass) {
+        print("Running test suite \(testClass)")
+        
+        let tests = testClass as! XCTestCase.Type
+        let testSuite = tests.defaultTestSuite()
+        testSuite.run()
+        let run = testSuite.testRun as! XCTestSuiteRun
+        
+        print("Ran \(run.executionCount) tests in \(run.testDuration)s with \(run.totalFailureCount) failures")
+    }
+}
+
+
+//: ## Tests
+
+class ExampleTests : XCTestCase {
+    
+    func testExample() {
+        // TODO:
+    }
+}
+
+
+//: ## Test execute
+
+TestRunner().run(ExampleTests.self)

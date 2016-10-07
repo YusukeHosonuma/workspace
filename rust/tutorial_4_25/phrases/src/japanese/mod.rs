ce06a18d022ce90069381d35787662ac5e0222be
Yusuke Hosonuma <tobi462@gmail.com>
2016-10-08 00:04:21 +0900

    4.25. クレートとモジュール　を追加

---
+++ rust/tutorial_4_25/phrases/src/japanese/mod.rs
@@ -0,0 +1,5 @@
+pub use self::greetings::hello;
+pub use self::farewells::goodbye;
+
+pub mod greetings;
+pub mod farewells;
---
+++ rust/tutorial_4_25/phrases/src/main.rs
@@ -0,0 +1,13 @@
+extern crate phrases as sayings; // リンク指示
+
+use sayings::japanese::greetings as ja_greetings;
+use sayings::japanese::farewells::*;
+use sayings::english::{self, greetings as en_greetings, farewells as en_farewells};
+
+fn main() {
+    println!("Hello in English: {}", en_greetings::hello());
+    println!("And in Japanese: {}", ja_greetings::hello());
+    println!("Goodbye in English: {}", english::farewells::goodbye());
+    println!("Again: {}", en_farewells::goodbye());
+    println!("And in Japanese: {}", goodbye());
+}

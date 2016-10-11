22a63f9c404ae6dbbeab354fab98225ff0a104e7
Yusuke Hosonuma <tobi462@gmail.com>
2016-10-12 00:22:24 +0900

    Refactor: 安全な型（enum）に落とし込んでから式を評価するように

--- Rust/RPN/src/main.rs
+++ Rust/RPN/src/main.rs
@@ -11,6 +11,19 @@ fn main() {
     assert_eq!(resolve("9 1 5 + 2 3 + *"), None); // スタックの数字を使い切らない
 }
 
+#[derive(Debug, PartialEq)]
+enum Expr {
+    Operator(OperatorType),
+    Num(i32),
+    Unknown,
+}
+
+#[derive(Debug, PartialEq)]
+enum OperatorType {
+    Sum,
+    Product,
+}
+
 /// RPN式を解く
 ///
 /// # Arguments
@@ -26,23 +39,42 @@ fn resolve(expr: &str) -> Option<i32> {
 
     let mut stack: Vec<i32> = Vec::new();
 
-    for s in expr.split(" ") {
+    // トークン解析
+    let expr: Vec<Expr> = expr.split(" ").map(|s| {
         if s == "+" {
-            if !pop2_apply(&mut stack, |a, b| a + b) { return None };
+            Expr::Operator(OperatorType::Sum)
         } else if s == "*" {
-            if !pop2_apply(&mut stack, |a, b| a * b) { return None };
+            Expr::Operator(OperatorType::Product)
         } else {
             match s.parse::<i32>() {
-                Ok(n)  => stack.push(n),
-                Err(_) => return None,
+                Ok(n)  => Expr::Num(n),
+                Err(_) => Expr::Unknown,
             }
+        }
+    }).collect();
+
+    // 未サポートのトークンがあれば失敗
+    if expr.contains(&Expr::Unknown) {
+        return None
+    }
+
+    // 式を評価
+    for e in expr {
+        match e {
+            Expr::Operator(op) => match op {
+                OperatorType::Sum     => if !pop2_apply(&mut stack, |a, b| a + b) { return None },
+                OperatorType::Product => if !pop2_apply(&mut stack, |a, b| a * b) { return None },
+            },
+            Expr::Num(n) => stack.push(n),
+            Expr::Unknown => panic!("Unknown token not resolved."),
         }
     }
 
+    // スタックに残り1つであれば、それが答え
     if stack.len() == 1 { stack.pop() } else { None }
 }
 
-fn pop2_apply<F: >(stack: &mut Vec<i32>, apply: F) -> bool where F: Fn(i32, i32) -> i32 {
+fn pop2_apply<F>(stack: &mut Vec<i32>, apply: F) -> bool where F: Fn(i32, i32) -> i32 {
     if let Some((a, b)) = pop2(stack) {
         stack.push(apply(a, b));
         true

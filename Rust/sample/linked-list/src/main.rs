cf5baca79f40d0c6d89373fcef3331aebeccc351
Yusuke Hosonuma <tobi462@gmail.com>
2016-10-22 20:40:13 +0900

    cons-listのサンプル

---
+++ Rust/sample/linked-list/src/main.rs
@@ -0,0 +1,49 @@
+// cons-list のサンプル
+
+use List::*;
+
+enum List {
+    Cons(u32, Box<List>), // タプルで表現（Boxで参照としてラップ）
+    Nil,
+}
+
+impl List {
+    fn new() -> List {
+        Nil
+    }
+
+    fn prepend(self, elem: u32) -> List {
+        Cons(elem, Box::new(self))
+    }
+
+    fn len(&self) -> u32 {
+        match *self {
+            // selfは借用したものなので、tailの所有権を得ることは出来ない。なので`ref`で参照を取得している。
+            Cons(_, ref tail) => 1 + tail.len(),
+            Nil => 0
+        }
+    }
+
+    fn stringify(&self) -> String {
+        match *self {
+            Cons(head, ref tail) => {
+                format!("{}, {}", head, tail.stringify())
+            },
+            Nil => {
+                format!("Nil")
+            },
+        }
+    }
+}
+
+fn main() {
+
+    let mut list = List::new();
+
+    list = list.prepend(1);
+    list = list.prepend(2);
+    list = list.prepend(3);
+
+    println!("linked list has length: {}", list.len());
+    println!("{}", list.stringify());
+}

54ada268f764e42478adf80b8cc3fae75c82c46e
Yusuke Hosonuma <tobi462@gmail.com>
2016-10-24 23:01:21 +0900

    mut属性のサンプル

---
+++ Rust/sample/mutability/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let _immutable_binding = 1;
+    let mut mutable_binding = 1;
+
+    println!("Before mutation: {}", mutable_binding);
+
+    mutable_binding += 1;
+
+    println!("After mutation: {}", mutable_binding);
+
+    // デフォルトでイミュータブルなので、再代入はNG
+    //_immutable_binding += 1;
+}

bb26acf37f61fe1b8483885e12619ba9cc1dc60e
Yusuke Hosonuma <tobi462@gmail.com>
2016-12-06 00:25:51 +0900

    C: UDPのサンプル

---
+++ C/UDP/udp_client.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int
+main(void)
+{
+    int sock;
+    struct sockaddr_in s;
+    int n;
+
+    // IPv4 / UDP
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+
+    s.sin_family = AF_INET;
+    s.sin_port = htons(11111);
+    s.sin_len = sizeof(s);
+
+    // 宛先を指定
+    n = inet_pton(AF_INET, "127.0.0.1", &s.sin_addr);
+    if (n < 1) {
+        perror("inet_pton");
+        return 1;
+    }
+
+    // 送信
+    n = sendto(sock, "TEST", 5, 0, (struct sockaddr *)&s, sizeof(s));
+    if (n < 1) {
+        perror("sendto");
+        return 1;
+    }
+
+    close(sock);
+    return 0;
+}
---
+++ C/UDP/udp_server.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+int
+main(void)
+{
+    int sock;
+    struct sockaddr_in bindaddr;
+
+    struct sockaddr_storage senderinfo;
+    socklen_t addrlen;
+    char buf[2048];
+    int n;
+
+    // IPv4 / UDP
+    sock = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sock < 0) {
+        perror("socket");
+        return 1;
+    }
+
+    bindaddr.sin_family = AF_INET;
+    bindaddr.sin_port = htons(11111);
+    bindaddr.sin_addr.s_addr = INADDR_ANY;
+    bindaddr.sin_len = sizeof(bindaddr);
+
+    if (bind(sock, (struct sockaddr *)&bindaddr, sizeof(bindaddr)) != 0) {
+        perror("bind");
+        return 1;
+    }
+
+    addrlen = sizeof(senderinfo);
+
+    // UDP受信
+    n = recvfrom(sock, buf, sizeof(buf) - 1, 0, (struct sockaddr *)&senderinfo, &addrlen);
+
+    write(fileno(stdout), buf, n);
+
+    close(sock);
+    return 0;
+}

3b016192ab228129ea400f2e8180746242f9f8bd
Yusuke Hosonuma <tobi462@gmail.com>
2017-01-04 18:01:36 +0900

    Scala: refactor - use pattern match instead of if else

--- Scala/Fib.scala
+++ Scala/Fib.scala
@@ -1,9 +1,14 @@
 // n番目のフィボナッチ数列を求める
 object Fib {
-  def fib(n:Int):Int = if (n < 3) n else fib(n - 2) + fib(n - 1)
-  def main(args:Array[String]) = {
+
+  def fib(n: Int): Int = n match {
+      case n if n < 3 => 1 // 1, 1, ...
+      case n => fib(n - 2) + fib(n - 1) // 2, 3, 5, ...
+  }
+
+  def main(args: Array[String]) = {
     (1 to 10).foreach { i =>
-      println(i + " : " + fib(i))
+      println(fib(i))
     }
   }
 }

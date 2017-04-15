cfafe91551f4fab42b23003f5e1e1507938803f8
Yusuke Hosonuma <tobi462@gmail.com>
2017-04-15 23:36:33 +0900

    ReactNative: テキストのスタイリング

--- ReactNative/HelloWorld/index.ios.js
+++ ReactNative/HelloWorld/index.ios.js
@@ -1,12 +1,67 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text } from 'react-native';
+import { AppRegistry, StyleSheet, Text, Image, View } from 'react-native';
 
-export default class HelloWorld extends Component {
+// export default class HelloWorld extends Component {
+//   render() {
+//     return (
+//       <Text>Hello world!</Text>
+//     );
+//   }
+// }
+
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+
+    // Reduxを使ったほうが良いとのこと？
+    this.state = { showText: true };
+    setInterval(() => {
+      this.setState({ showText: !this.state.showText })
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.showText ? this.props.text : ' ';
     return (
-      <Text>Hello world!</Text>
+      <Text>{display}</Text>
     );
   }
 }
 
-AppRegistry.registerComponent('HelloWorld', () => HelloWorld);
+class LotsOfStyles extends Component {
+  render() {
+    return (
+      <View>
+        <Text style={styles.red}>just red</Text>
+        <Text style={styles.bigblue}>just bigblue</Text>
+        <Text style={[styles.bigblue, styles.red]}>bigblue, then red</Text>
+        <Text style={[styles.red, styles.bigblue]}>red, then bigblue</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  bigblue: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+  },
+  red: {
+    color: 'red',
+  },
+})
+
+export default class Images extends Component {
+  render() {
+    return (
+      <View>
+        <Blink text='Apple' />
+        <Blink text='Orange' />
+        <Blink text='Banana' />
+      </View>
+    );
+  }
+}
+
+AppRegistry.registerComponent('HelloWorld', () => LotsOfStyles);
